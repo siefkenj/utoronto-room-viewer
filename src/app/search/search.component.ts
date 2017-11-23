2e35973056ce7d89b8c1e00c16c65f2b207f6227
daspore <paulstrauss84@gmail.com>
2017-11-22 22:08:13 -0500

    Convert date picker to Bootstrap implementation
    Replace angular-io-datepicker with ng2-datepicker (requires Bootstrap
    modules)

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbDateStruct, NgbDateParserFormatter } from "@ng-bootstrap/ng-bootstrap";
 
 import { Building } from '../models/building';
 import { BuildingService } from '../services/building.service';
 
 import { Room } from '../models/room';
 import { RoomService } from '../services/room.service';
+
+export class DateParserFormatter extends NgbDateParserFormatter {
+    parse(value: string): NgbDateStruct {
+      return {
+        year: +value.substring(0,4),
+        month: +value.substring(5,7),
+        day: +value.substring(8,10)
+      };
+    }
+    
+    format(date: NgbDateStruct): string {
+      return `${date.year}-${date.month}-${date.day}`;
+    }
+}
 
 @Component({
   selector: 'search',
@@ -14,7 +29,7 @@ import { RoomService } from '../services/room.service';
 })
 export class SearchComponent implements OnInit {
   // Date config options
-  date = new Date();
+  date: NgbDateStruct;
   
   // Capacity config options
   filterByCapacity = false;
@@ -63,20 +78,46 @@ export class SearchComponent implements OnInit {
   constructor(
     private buildingService: BuildingService,
     private roomService: RoomService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router,
+    private dateFormatter: DateParserFormatter) { }
 
   ngOnInit() {
+    this.date = this.dateFormatter.parse(this.convertDateToQueryParamString(new Date()));
     this.buildingService.getBuildings().then(bs => this.buildings = bs);
     this.roomService.getRooms().then(rs => this.allRooms = rs);
     this.updateQueryParams();
     
     // Retrieve the query parameters
     this.route.queryParamMap.subscribe(pmap => {
+      // Enable calendar if we have enough query parameters in the URL
       if (pmap.has('date') && pmap.has('buildings') && pmap.get('buildings'))
       {
         this.hasActiveQuery = true;
       } else {
         this.hasActiveQuery = false;
+      }
+      
+      // Update inputs based on query parameters
+      if (pmap.has('date')) {
+        this.date = this.dateFormatter.parse(pmap.get('date'));
+      }
+      if (pmap.has('buildings')) {
+        this.selectedBuildings = (pmap.get('buildings') || '').split(',');
+      }
+      if (pmap.has('rooms')) {
+        this.selectedRooms = (pmap.get('rooms') || '').split(',');
+      }
+      if (pmap.has('minCapacity') || pmap.has('maxCapacity')) {
+        var min = pmap.get('minCapacity');
+        if (min) {
+          this.capacityRange[0] = +min;
+        }
+        var max = pmap.get('maxCapacity');
+        if (max) {
+          this.capacityRange[1] = +max;
+        }
+        this.filterByCapacity = true;
       }
     });
   }
@@ -137,10 +178,16 @@ export class SearchComponent implements OnInit {
    * Helper function to convert a date object into a URL accessible string
    */ 
   private convertDateToQueryParamString(date) {
+    if (!date) {
+      return '';
+    }
+    
     if (date.format) {
       return `${date.format('YYYY-MM-DD')}`;
     } else if (date.getUTCFullYear && date.getUTCMonth && date.getDate) {
       return `${date.getUTCFullYear()}-${date.getUTCMonth()+1}-${date.getDate()}`;
+    } else if (date.year && date.month && date.day) {
+      return `${date.year}-${date.month}-${date.day}`;
     }
     return date.toString();
   }
