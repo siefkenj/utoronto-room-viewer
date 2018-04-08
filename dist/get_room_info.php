34f9fe1ba28aa6e1bd2d3e05c06ca2491a50662a
Jason Siefken <siefkenj@gmail.com>
2018-04-08 00:55:03 -0400

    Added workaround for OSM api miscalculating Sudnays

--- dist/get_room_info.php
+++ dist/get_room_info.php
@@ -67,6 +67,53 @@
 		echo json_encode($room_data);
 		exit();
 	}
+
+	// XXX the osm website lists Sunday listings as belonging to the previous week,
+	// but their API makes Sunday the start of the next week. If the day is a sunday,
+	// retrieve two weeks of data and merge it.
+	$year = substr($date, 0, 4);
+	$month = substr($date, 4, 2);
+	$day = substr($date, 6, 2);
+	date_default_timezone_set('UTC');
+	$weekday = date('w', strtotime("$year-$month-$day"));
+	if ($weekday == 0 || $weekday == 6) {
+		$orig_date = $date;
+		$curr_day = DateTime::createFromFormat("Y-m-d", "$year-$month-$day");
+		$curr_day->modify('-1 day');
+		// $date is now one day previous from where it was
+		$date = $curr_day->format('Ymd');
+
+		// XXX REPEATED CODE...but I can't be bothered to figure out functions right now...
+
+		// now grab scheduling data
+		$info_url = "http://www.osm.utoronto.ca/booking_request/f?p=200:3:::NO::P3_BLDG,P3_ROOM,P3_CALENDAR_DATE:$bldg,$room,$date";
+		// get the room info
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $info_url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		$roominfo_html = curl_exec($ch);
+		curl_close($ch);
+
+
+		$dom = new DOMDocument;
+		@$dom->loadHTML($roominfo_html);
+		$xpath = new DOMXpath($dom);
+
+		$date_elms = $xpath->query('//div[@id="apex_cal_data_grid_src"]/../input[1]');
+
+		$bookings = array();
+		for ($i = 0; $i < $date_elms->length; $i++) {
+			$booking_time = $date_elms->item($i)->getAttribute('value');
+			$elm = $xpath->query('../div[@id="apex_cal_data_grid_src"]', $date_elms->item($i));
+			$booking_details = trim($elm->item(0)->textContent);
+			$bookings[] = array("time" => $booking_time, "desc" => $booking_details, "id" => "$bldg $room $booking_time");
+		}
+		$room_data['bookings'] = $bookings;
+
+		$date = $orig_date;
+	}
+
 	// now grab scheduling data
 	$info_url = "http://www.osm.utoronto.ca/booking_request/f?p=200:3:::NO::P3_BLDG,P3_ROOM,P3_CALENDAR_DATE:$bldg,$room,$date";
 	// get the room info
